f44c2b5867d54bf1eca4aa881427a60abfd3dfcd
a4121 <a41213908@gmail.com>
2020-12-14 10:57:22 +0900

    Initail Commit

---
+++ src/main/java/com/stock/stockhere/StockhereApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stockhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockhereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockhereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stock/stockhere/StockhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.stockhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockhereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
